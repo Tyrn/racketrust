1441bea233a008c378248471a0f85fb0b76b9a85
Tyrn <a@b.com>
2024-03-14 19:09:04 +0300

    fmtall and walk.rkt added

---
+++ walk.rkt
@@ -0,0 +1,36 @@
+#!/usr/bin/env racket
+#lang racket
+
+(require racket/path
+         racket/stream
+         racket/file)
+
+; Function to list all files and directories in a directory
+(define (children parent)
+  (define-values (all-items) (directory-list parent #:build? #t))
+  (let-values ([(dirs files) (partition directory-exists? all-items)])
+    (values dirs files)))
+
+;; Function to traverse directories and produce a flatand lazy stream of files
+(define (traverse parent)
+  (define-values (dirs files) (children parent))
+  (stream-append (apply stream-append
+                        (for/list ([dir (in-list dirs)])
+                          (stream-lazy (traverse dir))))
+                 files))
+
+(define reds (stream-cons "red" reds))
+
+; Main function to traverse directories and print matching files
+(define (traverse-and-print)
+  (define-values (dirs files) (children "."))
+  (displayln (format "dirs: ~a" dirs))
+  (displayln (format "files: ~a" files))
+
+  ;(stream-for-each displayln (traverse ".")))
+  ;(stream-for-each displayln reds))
+  (for ([item (in-stream (traverse "."))] [idx (in-naturals 1)])
+    (printf "~a ~a\n" idx item)))
+
+; Run the main function
+(traverse-and-print)
