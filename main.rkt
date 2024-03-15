827b2e76d65c0e1cfbf2e5efbdee8b91cfef7614
Tyrn <a@b.com>
2024-03-15 18:53:36 +0300

    AI generated initials source to ai.rkt

--- main.rkt
+++ main.rkt
@@ -32,10 +32,7 @@
 
 ;; Code here
 (require racket/cmdline
-         racket/port
-         racket/string
-         racket/list
-         racket/set)
+         racket/port)
 (define who (box "world"))
 (define (pipediput)
   (for ([line (port->lines)])
@@ -45,92 +42,6 @@
               [("-p" "--pipe") "greet piped list" (pipediput)]
               #:args ()
               (printf "hello ~a~n" (unbox who)))
-
-(define (string-blank? str)
-  regexp-match-exact?
-  #px"\\s*"
-  str)
-
-(define (form-initial name)
-  "Makes an initial out of name,
-  handling special cases like von, Mc, O', etc."
-  (let ([cut (string-split name "'")])
-    (cond
-      ;; Deal with O'Connor and d'Artagnan.
-      [(and (> (length cut) 1) (not (non-empty-string? (list-ref cut 1))))
-       (cond
-         [(and (char-lower-case? (string-ref (list-ref cut 1) 0)))
-          (string-upcase (string (string-ref (list-ref cut 0) 0)))]
-         [else (string-append (list-ref cut 0) "'" (list-ref cut 1))])]
-      ;; Deal with Leonardo DiCaprio and Jackie McGee.
-      [(and (> (length name) 1) (ormap char-upper-case? (rest name)))
-       (let loop ([tail (rest name)] [prefix (first name)])
-         (if (char-upper-case? (first tail))
-             (string-append prefix (string (first tail)))
-             (loop (rest tail) (string-append prefix (string (first tail))))))]
-      [(set-member? #{"von"
-                      "фон"
-                      "van"
-                      "ван"
-                      "der"
-                      "дер"
-                      "til"
-                      "тиль"
-                      "zu"
-                      "цу"
-                      "af"
-                      "аф"
-                      "of"
-                      "из"
-                      "de"
-                      "де"
-                      "des"
-                      "дез"
-                      "del"
-                      "дель"
-                      "dos"
-                      "душ"
-                      "дос"
-                      "du"
-                      "дю"
-                      "la"
-                      "ла"
-                      "ля"
-                      "le"
-                      "ле"
-                      "haut"
-                      "от"}
-                    name)
-       (string (first name))]
-      [else (string-upcase (string (first name)))])))
-
-(define (initials authors)
-  "Reduces comma separated list of
-  authors to initials."
-  (string-join
-   (map
-    (lambda (author)
-      (let* ([replaced
-              (regexp-replace* #px"\"(?:\\.|\\[^\"\\])*\"" authors " ")] ;; Remove quoted substrings.
-             [odd-quotes-replaced (regexp-replace* #px"\"" replaced " ")] ;; Remove odd quotes.
-             [split-authors (string-split odd-quotes-replaced ",")]
-             [filtered-authors (filter (lambda (author)
-                                         (not (string-blank? (regexp-replace* #px"[-.]+" author ""))))
-                                       split-authors)]
-             [formatted-authors
-              (map (lambda (author)
-                     (let* ([split-barrels (string-split author "-")]
-                            [filtered-barrels
-                             (filter (lambda (barrel)
-                                       (not (string-blank? (regexp-replace* #px"[.]+" + barrel ""))))
-                                     split-barrels)]
-                            ;[raw-initials (map form-initial filtered-barrels)]
-                            [raw-initials filtered-barrels]
-                            [joined-barrels (string-join raw-initials ".")])
-                       joined-barrels))
-                   filtered-authors)])
-        (string-join formatted-authors "-")))
-    (string-split authors ","))))
 
 (module+ test
   ;; Any code in this `test` submodule runs when this file is run using DrRacket
