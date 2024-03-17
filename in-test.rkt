2a215eff627bf47c35ddb60c11a194e1fd0eca58
Tyrn <a@b.com>
2024-03-17 18:32:12 +0300

    First attempt at getting a list of test inputs

--- in-test.rkt
+++ in-test.rkt
@@ -3,4 +3,11 @@
 (require rackunit
          "smallin.rkt")
 
+(define initials-block
+  (list '("ron reagan-smith" "R.R-S.") '("Nell Guinn" "N.G.") '("ben lazar" "BL.")))
+
 (check-equal? (initials "ron reagan-smith") "R.R-S." "First try")
+
+(for ([i initials-block])
+  (check-equal? (initials (car i)) (car (cdr i)) "initials test")
+  (printf "'~a' '~a'\n" (car i) (car (cdr i))))
