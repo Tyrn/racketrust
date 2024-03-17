308de607f78407492fe4fff70ac6edcb27c29f5e
Tyrn <a@b.com>
2024-03-17 20:25:51 +0300

    Pattern matching employed

--- in-test.rkt
+++ in-test.rkt
@@ -1,13 +1,16 @@
 #lang racket/base
 
 (require rackunit
+         racket/match
          "smallin.rkt")
 
+(define (block-check data-block [message ""] [print? #f])
+  (for ([i data-block])
+    (match-define (list in out) i)
+    (check-equal? (initials in) out message)
+    (when print? (printf "'~a' '~a'\n" in out))))
+
 (define initials-block
-  (list '("ron reagan-smith" "R.R-S.") '("Nell Guinn" "N.G.") '("ben lazar" "BL.")))
+  (list '("ron reagan-smith" "R.R-S.") '("Nell Guinn" "N.G.") '("ben lazar" "B.L.")))
 
-(check-equal? (initials "ron reagan-smith") "R.R-S." "First try")
-
-(for ([i initials-block])
-  (check-equal? (initials (car i)) (car (cdr i)) "initials test")
-  (printf "'~a' '~a'\n" (car i) (car (cdr i))))
+(block-check initials-block "initials test" #t)
