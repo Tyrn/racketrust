19b7d3f7ed3a54a0a97b9bec90c0555321a6840c
Tyrn <a@b.com>
2024-03-18 02:46:14 +0300

    + test added

--- in-test.rkt
+++ in-test.rkt
@@ -11,8 +11,10 @@
     (when print?
       (printf "~v ~v\n" in out))))
 
+(define plus-block '(((2 2) 4) ((3 4) 7) ((1 1 1) 3)))
+(block-check + plus-block "+" #t)
+
 (define initials-block
   '((("ron reagan-smith") "R.R-S.") (("Nell Guinn") "N.G.")
                                     (("john ronald reuel Tolkien") "J.R.R.T.")))
-
-(block-check small:initials initials-block "initials test" #t)
+(block-check small:initials initials-block "initials" #t)
