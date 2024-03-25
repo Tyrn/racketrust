38ecffee272608397ba6f5a208ada77b11b33572
Tyrn <a@b.com>
2024-03-25 09:07:08 +0300

    srfi format introduced

--- helpers.rkt
+++ helpers.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require srfi/48)
 
 (define UNIT-LIST '(("" . 0) ("kB" . 0) ("MB" . 1) ("GB" . 2) ("TB" . 2) ("PB" . 2)))
 
@@ -9,9 +10,9 @@
      (let* ([xp (min (inexact->exact (log fb 1024.0)) (- (length UNIT-LIST) 1))]
             [qt (/ fb (expt 1024.0 xp))])
        (cond
-         [(= (cdr (list-ref UNIT-LIST xp)) 0) (format ".0f~a~a" qt (car (list-ref UNIT-LIST xp)))]
-         [(= (cdr (list-ref UNIT-LIST xp)) 1) (format ".1f~a~a" qt (car (list-ref UNIT-LIST xp)))]
-         [(= (cdr (list-ref UNIT-LIST xp)) 2) (format ".2f~a~a" qt (car (list-ref UNIT-LIST xp)))]
+         [(= (cdr (list-ref UNIT-LIST xp)) 0) (format "~0,0F~a" qt (car (list-ref UNIT-LIST xp)))]
+         [(= (cdr (list-ref UNIT-LIST xp)) 1) (format "~0,1F~a" qt (car (list-ref UNIT-LIST xp)))]
+         [(= (cdr (list-ref UNIT-LIST xp)) 2) (format "~0,2F~a" qt (car (list-ref UNIT-LIST xp)))]
          [else (error "Fatal error: human-fine(): unexpected decimals count.")]))]
     [(= bytes 0) "0"]
     [(= bytes 1) "1"]
