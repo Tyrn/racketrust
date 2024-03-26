d426232b9c5ec2b78ba102a8f4f37b67d0d867dd
Tyrn <a@b.com>
2024-03-26 07:39:00 +0300

    human-fine, list to vector

--- helpers.rkt
+++ helpers.rkt
@@ -4,14 +4,15 @@
 (provide human-fine)
 
 (define (human-fine bytes)
-  (define XB '(("" . 0) ("kB" . 0) ("MB" . 1) ("GB" . 2) ("TB" . 2) ("PB" . 2)))
+  (define XB '#(("" . 0) ("kB" . 0) ("MB" . 1) ("GB" . 2) ("TB" . 2) ("PB" . 2)))
   (cond
     [(> bytes 1)
-     (let* ([xp (min (exact-floor (log bytes 1024)) (- (length XB) 1))] [qt (/ bytes (expt 1024 xp))])
+     (let* ([xp (min (exact-floor (log bytes 1024)) (- (vector-length XB) 1))]
+            [qt (/ bytes (expt 1024 xp))])
        (cond
-         [(= (cdr (list-ref XB xp)) 0) (~a (~r qt #:precision 0) (car (list-ref XB xp)))]
-         [(= (cdr (list-ref XB xp)) 1) (format "~0,1F~a" qt (car (list-ref XB xp)))]
-         [(= (cdr (list-ref XB xp)) 2) (format "~0,2F~a" qt (car (list-ref XB xp)))]
+         [(= (cdr (vector-ref XB xp)) 0) (~a (~r qt #:precision 0) (car (vector-ref XB xp)))]
+         [(= (cdr (vector-ref XB xp)) 1) (format "~0,1F~a" qt (car (vector-ref XB xp)))]
+         [(= (cdr (vector-ref XB xp)) 2) (format "~0,2F~a" qt (car (vector-ref XB xp)))]
          [else (error "Fatal error: human-fine(): unexpected decimals count.")]))]
     [(= bytes 0) "0"]
     [(= bytes 1) "1"]
